375e7c9f87e52b1af3700ea071a53b82572ee7a0
Oscar Pérez <mr.omiguelperez@gmail.com>
2017-07-01 12:07:04 -0500

    Formulario para crear productos

--- Shoppy/products/urls.py
+++ Shoppy/products/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^$', views.products, name='products'),
     url(r'^detail/(?P<product_id>[0-9]+)/',
         views.product_detail, name='product_detail'),
+    url(r'^new/$', views.product_new, name='product_new'),
 ]
--- Shoppy/products/views.py
+++ Shoppy/products/views.py
@@ -1,5 +1,8 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
+from .forms import ProductForm
 from .models import Product
 
 
@@ -16,3 +19,17 @@ def product_detail(request, product_id):
         'product': product
     }
     return render(request, 'product_detail.html', context)
+
+
+def product_new(request):
+    form = ProductForm(request.POST or None, request.FILES or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            product = form.save(commit=False)
+            product.save()
+            return HttpResponseRedirect(reverse('product_detail', kwargs={'product_id': product.pk}))
+    context = {
+        'title': 'New product',
+        'form': form
+    }
+    return render(request, 'product_new.html', context)
